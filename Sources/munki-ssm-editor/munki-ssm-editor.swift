67c80262c01110fc370c286201e60048922c4e30
Jim Zajkowski <jamesez@umich.edu>
2022-06-29 14:52:20 -0400

    actual loading and saving

--- Sources/munki-ssm-editor/munki-ssm-editor.swift
+++ Sources/munki-ssm-editor/munki-ssm-editor.swift
@@ -1,5 +1,44 @@
 import ArgumentParser
 import Darwin
+import Foundation
+
+let SSMURL = URL(fileURLWithPath: "/Library/Managed Installs/manifests/SelfServeManifest")
+
+struct SSMEntries {
+    var installs: [String] = []
+    var uninstalls: [String] = []
+    
+    mutating func load() throws {
+        do {
+            let data = try Data(contentsOf: SSMURL)
+            if let ssm = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String:[String]] {
+                installs = ssm["managed_installs"]!
+                uninstalls = ssm["managed_uninstalls"]!
+            }
+        } catch {
+            print(error)
+            throw ExitCode.init(-1)
+        }
+    }
+    
+    func save() throws {
+        let plist = [ "managed_installs": self.installs,
+                      "managed_uninstalls": self.uninstalls ]
+        
+        if let stream = OutputStream.init(url: SSMURL, append: false) {
+            stream.open()
+            let written = PropertyListSerialization.writePropertyList(plist, to: stream, format: .xml, options: 0, error: nil)
+            
+            if written == 0 {
+                print("Couldn't write \(SSMURL.path) - are you not root?")
+                throw ExitCode.init(-2)
+            }
+        } else {
+            print("Couldn't open \(SSMURL.path) for writing")
+            throw ExitCode.init(-2)
+        }
+    }
+}
 
 @main
 struct MunkiSSMEditor: ParsableCommand {
@@ -19,16 +58,6 @@ struct MunkiSSMEditor: ParsableCommand {
         // subcommand is not given on the command line.
         defaultSubcommand: Add.self
     )
-    
-    static var entries: [String] = []
-    
-    static func loadEntries() {
-        entries = [ "A", "B", "packagename", "C" ]
-    }
-    
-    static func saveEntries() {
-        print("saving \(entries)")
-    }
 }
 
 struct Options: ParsableArguments {
@@ -47,17 +76,18 @@ extension MunkiSSMEditor {
         @Argument
         var packageName: String
         
-        mutating func run() {
-            loadEntries()
+        mutating func run() throws {
+            var entries = SSMEntries()
+            try entries.load()
             
-            if entries.contains(packageName) == false {
+            if entries.installs.contains(where: { $0.lowercased() == packageName.lowercased() }) == false {
                 if options.verbose { print("Adding \(packageName)") }
-                entries.append(packageName)
+                entries.installs.append(packageName)
             } else {
                 if options.verbose { print("\(packageName) already in SSM") }
             }
 
-            saveEntries()
+            try entries.save()
         }
     }
 }
@@ -65,29 +95,43 @@ extension MunkiSSMEditor {
 extension MunkiSSMEditor {
     struct Remove: ParsableCommand {
         static var configuration = CommandConfiguration(
-            abstract: "Remove a package name"
+            abstract: "Remove a package from the install list"
         )
 
         @OptionGroup var options: Options
+        
+        @Flag(help: "Add the removed package to the uninstall list")
+        var uninstall: Bool = false
 
         @Argument
         var packageName: String
         
-        mutating func run() {
-            loadEntries()
-            
-            if entries.contains(packageName) {
+        mutating func run() throws {
+            var entries = SSMEntries()
+            try entries.load()
+
+            if entries.installs.contains(where: { $0.lowercased() == packageName.lowercased() }) {
                 if options.verbose { print("Removing \(packageName)") }
 
-                entries.removeAll() { entry in
-                    entry == packageName
+                entries.installs.removeAll() { entry in
+                    entry.lowercased() == packageName.lowercased()
+                }
+                
+                if uninstall {
+                    if entries.uninstalls.contains(where: { $0.lowercased() == packageName.lowercased() }) == false {
+                        entries.uninstalls.append(packageName)
+                        if options.verbose { print("...adding \(packageName) to managed_uninstalls") }
+                    } else {
+                        if options.verbose { print("\(packageName) already in managed_uninstalls")}
+                    }
                 }
 
             } else {
                 if options.verbose { print("\(packageName) not in SSM") }
             }
 
-            saveEntries()
+            try entries.save()
+            
         }
     }
 }
@@ -100,30 +144,38 @@ extension MunkiSSMEditor {
         
         @OptionGroup var options: Options
 
+        @Flag(help: "Add the removed package to the uninstall list")
+        var uninstall: Bool = false
+
         @Argument
         var oldPackageName: String
         
         @Argument
         var newPackageName: String
         
-        mutating func run() {
-            loadEntries()
-            
-            if entries.contains(oldPackageName) == false {
+        mutating func run() throws {
+            var entries = SSMEntries()
+            try entries.load()
+
+            if entries.installs.contains(where: { $0.lowercased() == oldPackageName.lowercased() }) == false {
                 if options.verbose { print("\(oldPackageName) not in SSM, can't replace") }
                 return
             }
             
-            entries = entries.map { entry in
-                if entry == oldPackageName {
-                    if options.verbose { print("swapping \(oldPackageName) to \(newPackageName)") }
+            entries.installs = entries.installs.map { entry in
+                if entry.lowercased() == oldPackageName.lowercased() {
+                    if options.verbose { print("replacing \(oldPackageName) with \(newPackageName)") }
+                    if uninstall {
+                        if options.verbose { print("...adding \(oldPackageName) to managed_uninstalls") }
+                        entries.uninstalls.append(oldPackageName)
+                    }
                     return newPackageName
                 } else {
                     return entry
                 }
             }
-            
-            saveEntries()
+
+            try entries.save()
         }
     }
 }
